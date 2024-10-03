8535c5e71492b58e236975fa879c3b1f84bc4c1f
SeethapathiLab <nidhise@mit.edu>
2024-10-03 12:40:13 -0400

    Delete ComprehensiveCode directory

--- ComprehensiveCode/simulateIPUntilEndstance.m
+++
@@ -1,9 +0,0 @@
-function [tlistTillEndstance, statevarlistTillEndstance] = ...
-    simulateIPUntilEndstance(statevar0,tspan,paramFixed,paramController)
-
-% simulating inverted pendulum till midstance
-options = odeset('Events',@DetectEndstance,'reltol',1e-10,'abstol',1e-10);
-[tlistTillEndstance,statevarlistTillEndstance] = ...
-    ode45(@singlePendulumODE,tspan,statevar0,options,paramFixed,paramController);
-
-end % checked and essential
--- ComprehensiveCode/simulateIPUntilMidstance.m
+++
@@ -1,9 +0,0 @@
-function [tlistTillMidstance, statevarlistTillMidstance] = ...
-    simulateIPUntilMidstance(statevar0,tspan,paramFixed,paramController)
-
-% simulating inverted pendulum till midstance
-options = odeset('Events',@DetectMidstance,'reltol',1e-10,'abstol',1e-10);
-[tlistTillMidstance,statevarlistTillMidstance] = ...
-    ode45(@singlePendulumODE,tspan,statevar0,options,paramFixed,paramController);
-
-end % checked and essential
